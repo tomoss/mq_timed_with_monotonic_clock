4234179ef83d1cadc8026145f7d500d35158452c
tomoss <ionut@pop-os.localdomain>
2022-01-08 22:19:52 +0200

    test commit

--- mq_monotonic.h
+++ mq_monotonic.h
@@ -5,10 +5,14 @@ static ssize_t mq_timedreceive_monotonic(mqd_t mqdes, char *msg_ptr, size_t msg_
 {
     ssize_t ret = -1;
     errno = ETIMEDOUT;
+
+    return ret;
 }
 
 static int mq_timedsend_monotonic(mqd_t mqdes, const char *msg_ptr, size_t msg_len, unsigned int msg_prio, const struct timespec *abs_timeout)
 {
     int ret = -1;
     errno = ETIMEDOUT;
+
+    return ret;
 }
