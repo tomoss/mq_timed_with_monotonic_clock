ed73f6ed92f21d84deb0a90e8b7b4463b172c023
tomoss <ionut@pop-os.localdomain>
2022-01-08 22:08:05 +0200

    init

---
+++ mq_monotonic.h
@@ -0,0 +1,14 @@
+#include <mqueue.h>
+#include <cerrno>
+
+static ssize_t mq_timedreceive_monotonic(mqd_t mqdes, char *msg_ptr, size_t msg_len, unsigned *msg_prio, const struct timespec *abs_timeout)
+{
+    ssize_t ret = -1;
+    errno = ETIMEDOUT;
+}
+
+static int mq_timedsend_monotonic(mqd_t mqdes, const char *msg_ptr, size_t msg_len, unsigned int msg_prio, const struct timespec *abs_timeout)
+{
+    int ret = -1;
+    errno = ETIMEDOUT;
+}
